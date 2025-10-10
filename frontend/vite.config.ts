0012e5dfee11e3648a8b1154486a30947202a966
Yasiru <yasirubhagya@gmail.com>
2025-10-10 23:47:53 +0200

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,12 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  server: {
+    port: 3000,        //change the dev server port
+    host: true,        //allow external access (needed in Docker)
+    watch: {
+      usePolling: true,     //force file polling
+      interval: 100,        // optional, check every 100ms
+    },
+  },
 });
